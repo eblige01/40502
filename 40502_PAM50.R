07a0caf7f962646800fee286b67c503eee31a463
Your Name <your.email@example.com>
2025-02-07 11:43:17 -0500

    Finishing PAM50 Vs modules

--- 40502_PAM50.R
+++ 40502_PAM50.R
@@ -47,39 +47,113 @@ mergeddata_tils_sub$sTILs_cat <- ifelse(mergeddata_tils_sub$sTILs >= 5,"High","L
 #          rot_labels = 30),  # Rotate labels by 45 degrees
 #        gp_legend = gpar(fontsize = 6))  # Font size for the legend labels
 
-# PAM50 vs RNA Decon estimates
-### Subsetting duplicate RNA seq sample IDs 
-unique_PAM <- mergeddata %>%
-  distinct(rna_decon_sampleid, .keep_all = TRUE)
+# ## PAM50 vs RNA Decon estimates
+# ### Subsetting duplicate RNA seq sample IDs 
+# unique_PAM_rna <- mergeddata %>%
+#   distinct(rna_decon_sampleid, .keep_all = TRUE)
+# 
+# # Reformatting and merging
+# trans_rna_seq_df <- as.data.frame(t(rna_seq_df))
+# 
+# trans_rna_seq_df$rna_decon_sampleid <- row.names(trans_rna_seq_df)
+# 
+# # Change this line for other analysis TILS/PAM50
+# trans_rna_seq_df <- merge(trans_rna_seq_df, unique_PAM_rna[, c("rna_decon_sampleid", "Call")], by = "rna_decon_sampleid")
+# row.names(trans_rna_seq_df) <- trans_rna_seq_df$rna_decon_sampleid
+# trans_rna_seq_df <- trans_rna_seq_df %>% select (-"rna_decon_sampleid")
+# 
+# # Empty data frame for results
+# PAM50_results <- data.frame(Gene = character(), p_value = numeric(), stringsAsFactors = FALSE, significant_pairs = character())
+# 
+# # Get the gene column names (excluding sample_id and Call)
+# gene_cols <- setdiff(names(trans_rna_seq_df), c("rna_decon_sampleid", "Call"))
+# 
+# # Loop through each gene column
+# for (gene in gene_cols) {
+#   # Ensure the column is numeric
+#   trans_rna_seq_df[[gene]] <- as.numeric(trans_rna_seq_df[[gene]])
+# 
+#   # Perform Kruskal-Wallis test
+#   test_result <- kruskal.test(trans_rna_seq_df[[gene]] ~ trans_rna_seq_df$Call)
+# 
+#   # If Kruskal-Wallis test is significant, perform Dunn's test
+#   significant_pairs <- NA_character_
+#   if (test_result$p.value < 0.05) {
+#     dunn_result <- dunnTest(trans_rna_seq_df[[gene]] ~ trans_rna_seq_df$Call, method = "bh")
+# 
+#     # Check column names and extract significant pairwise comparisons
+#     if ("Comparison" %in% colnames(dunn_result$res) & "P.adj" %in% colnames(dunn_result$res)) {
+#       sig_pairs <- dunn_result$res %>%
+#         filter(P.adj < 0.05) %>%
+#         pull(Comparison)  # Extract significant pair names
+# 
+#       # Store significant comparisons as a comma-separated string
+#       if (length(sig_pairs) > 0) {
+#         significant_pairs <- paste(sig_pairs, collapse = ", ")
+#       }
+#     }
+#   }
+# 
+#   # Append results to module_results dataframe
+#   PAM50_results <- rbind(PAM50_results,
+#                           data.frame(Gene = gene,
+#                                      p_value = test_result$p.value,
+#                                      significant_pairs = significant_pairs))
+# }
+# 
+# # Adjust p-values
+# PAM50_results$p_adj <- p.adjust(PAM50_results$p_value, method = "bonferroni")
+# 
+# # Subset significant module_results after adjustment (e.g., FDR < 0.05)
+# PAM50_results_sig <-PAM50_results %>% filter(p_adj < 0.05)
+# #Saving results
+#  write_xlsx(PAM50_results_sig, "PAM50_decon.xlsx")
 
-# Reformatting and merging
-trans_rna_seq_df <- as.data.frame(t(rna_seq_df))
+### Module Analysis 
 
-trans_rna_seq_df$rna_decon_sampleid <- row.names(trans_rna_seq_df)
+signature_data <- read.table("/Users/eblige99/Desktop/Research/StoverLab_rotation/data/cdt.txt", header = TRUE, sep = "\t", comment.char = "", quote = "")
 
-# Change this line for other analysis TILS/PAM50
-trans_rna_seq_df <- merge(trans_rna_seq_df, unique_PAM[, c("rna_decon_sampleid", "Call")], by = "rna_decon_sampleid")
-row.names(trans_rna_seq_df) <- trans_rna_seq_df$rna_decon_sampleid
-trans_rna_seq_df <- trans_rna_seq_df %>% select (-"rna_decon_sampleid")
+# Reformatting signature data for analysis 
+# Removing unnecessary rows and coloumns
+resignature_data <- signature_data %>% select(-c("GID","CLID","GWEIGHT"))
+resignature_data <- resignature_data %>% slice(-c(1,2))
 
-# Empty data frame for results
-PAM50_results <- data.frame(Gene = character(), p_value = numeric(), stringsAsFactors = FALSE, significant_pairs = character())
+# Transposing the dataframe
+resignature_data <- t(resignature_data)
+
+# Making first row colnames and making rownames a column 
+colnames(resignature_data) <- resignature_data[1,]
+resignature_data <- resignature_data[-1,]
+resignature_data <- as.data.frame(resignature_data)
+resignature_data$INVESTIGATOR_SAMPLENAME <- rownames(resignature_data)
+
+# Moving INVESTIGATOR_SAMPLENAME for to the from for convenience 
+resignature_data <- resignature_data[, c("INVESTIGATOR_SAMPLENAME", setdiff(names(resignature_data), "INVESTIGATOR_SAMPLENAME"))]
+
+# Reformatting and merging 
+resignature_data <- resignature_data %>% mutate(INVESTIGATOR_SAMPLENAME = sub("^X", "", INVESTIGATOR_SAMPLENAME))
+
+resignature_data <- merge(resignature_data,mergeddata[, c("INVESTIGATOR_SAMPLENAME", "Call")],by = "INVESTIGATOR_SAMPLENAME",all.y=TRUE)
+# PAM50 vs Modules
+#Initialize an empty dataframe to store module_results
+
+module_results <- data.frame(Gene = character(), p_value = numeric(), stringsAsFactors = FALSE, significant_pairs = character())
 
 # Get the gene column names (excluding sample_id and Call)
-gene_cols <- setdiff(names(trans_rna_seq_df), c("rna_decon_sampleid", "Call"))
+gene_cols <- setdiff(names(resignature_data), c("INVESTIGATOR_SAMPLENAME", "Call"))
 
 # Loop through each gene column
 for (gene in gene_cols) {
   # Ensure the column is numeric
-  trans_rna_seq_df[[gene]] <- as.numeric(trans_rna_seq_df[[gene]])
+  resignature_data[[gene]] <- as.numeric(resignature_data[[gene]])
 
   # Perform Kruskal-Wallis test
-  test_result <- kruskal.test(trans_rna_seq_df[[gene]] ~ trans_rna_seq_df$Call)
+  test_result <- kruskal.test(resignature_data[[gene]] ~ resignature_data$Call)
 
   # If Kruskal-Wallis test is significant, perform Dunn's test
   significant_pairs <- NA_character_
   if (test_result$p.value < 0.05) {
-    dunn_result <- dunnTest(trans_rna_seq_df[[gene]] ~ trans_rna_seq_df$Call, method = "bh")
+    dunn_result <- dunnTest(resignature_data[[gene]] ~ resignature_data$Call, method = "bh")
 
     # Check column names and extract significant pairwise comparisons
     if ("Comparison" %in% colnames(dunn_result$res) & "P.adj" %in% colnames(dunn_result$res)) {
@@ -95,17 +169,24 @@ for (gene in gene_cols) {
   }
 
   # Append results to module_results dataframe
-  PAM50_results <- rbind(PAM50_results,
+  module_results <- rbind(module_results,
                           data.frame(Gene = gene,
                                      p_value = test_result$p.value,
                                      significant_pairs = significant_pairs))
 }
 
-# Adjust p-values
-PAM50_results$p_adj <- p.adjust(PAM50_results$p_value, method = "bonferroni")
+# Adjust p-values using Benjamini-Hochberg correction
+module_results$p_adj <- p.adjust(module_results$p_value, method = "BH")
 
 # Subset significant module_results after adjustment (e.g., FDR < 0.05)
-PAM50_results_sig <-PAM50_results %>% filter(p_adj < 0.05)
-#Saving results
- write_xlsx(PAM50_results_sig, "PAM50_decon.xlsx")
+significant_module_results <- module_results %>% filter(p_adj < 0.05)
+
+# Reordering significant module_results
+significant_module_results <- significant_module_results %>%
+  mutate(PMID = ifelse(grepl("_PMID\\.\\d+$", Gene),
+                       sub(".*_PMID\\.", "", Gene),
+                       NA_character_)) %>%
+  arrange(desc(!is.na(PMID)), PMID)
+# Saving significant module_results as a excel file
+write_xlsx(significant_module_results, "PAM50_gene_signatures.xlsx")
 
