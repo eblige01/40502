36a81f16bac3fc849345a29c2c0b4c7c1390b25b
Your Name <your.email@example.com>
2025-02-07 11:44:23 -0500

    Removing PAM50 code

--- 40502_Immune.R
+++ 40502_Immune.R
@@ -95,52 +95,6 @@ trans_rna_seq_df <- trans_rna_seq_df %>% select (-"rna_decon_sampleid")
 # #Saving results
 # write_xlsx(significant_rna_results, "sTILs_decon.xlsx")
 
-#PAM50 vs Immune
-# PAM50_results <- data.frame(Gene = character(), p_value = numeric(), stringsAsFactors = FALSE, significant_pairs = character())
-# 
-# # Get the gene column names (excluding sample_id and Call)
-# gene_cols <- setdiff(names(trans_rna_seq_df), c("rna_decon_sampleid", "Call"))
-# 
-# # Loop through each gene column
-# for (gene in gene_cols) {
-#   # Ensure the column is numeric
-#   trans_rna_seq_df[[gene]] <- as.numeric(trans_rna_seq_df[[gene]])
-# 
-#   # Perform Kruskal-Wallis test
-#   test_result <- kruskal.test(trans_rna_seq_df[[gene]] ~ trans_rna_seq_df$Call)
-# 
-#   # If Kruskal-Wallis test is significant, perform Dunn's test
-#   significant_pairs <- NA_character_
-#   if (test_result$p.value < 0.05) {
-#     dunn_result <- dunnTest(trans_rna_seq_df[[gene]] ~ trans_rna_seq_df$Call, method = "bh")
-# 
-#     # Check column names and extract significant pairwise comparisons
-#     if ("Comparison" %in% colnames(dunn_result$res) & "P.adj" %in% colnames(dunn_result$res)) {
-#       sig_pairs <- dunn_result$res %>%
-#         filter(P.adj < 0.05) %>%
-#         pull(Comparison)  # Extract significant pair names
-# 
-#       # Store significant comparisons as a comma-separated string
-#       if (length(sig_pairs) > 0) {
-#         significant_pairs <- paste(sig_pairs, collapse = ", ")
-#       }
-#     }
-#   }
-# 
-#   # Append results to module_results dataframe
-#   PAM50_results <- rbind(PAM50_results,
-#                           data.frame(Gene = gene,
-#                                      p_value = test_result$p.value,
-#                                      significant_pairs = significant_pairs))
-# }
-# 
-# # Adjust p-values 
-# PAM50_results$p_adj <- p.adjust(PAM50_results$p_value, method = "bonferroni")
-# 
-# # Subset significant module_results after adjustment (e.g., FDR < 0.05)
-# PAM50_results_sig <-PAM50_results %>% filter(p_adj < 0.05)
-# #Saving results
-#  write_xlsx(PAM50_results_sig, "PAM50_decon.xlsx")
 
 # # Spearman correlation 
 # ## Convert columns to numeric 
@@ -279,61 +233,6 @@ resignature_data <- merge(resignature_data,uniqueData[, c("INVESTIGATOR_SAMPLENA
 
 
 
-# PAM50 vs Modules
-# Initialize an empty dataframe to store module_results
-# 
-# module_results <- data.frame(Gene = character(), p_value = numeric(), stringsAsFactors = FALSE, significant_pairs = character())
-# 
-# # Get the gene column names (excluding sample_id and Call)
-# gene_cols <- setdiff(names(resignature_data), c("INVESTIGATOR_SAMPLENAME", "Call"))
-# 
-# # Loop through each gene column
-# for (gene in gene_cols) {
-#   # Ensure the column is numeric
-#   resignature_data[[gene]] <- as.numeric(resignature_data[[gene]])
-#   
-#   # Perform Kruskal-Wallis test
-#   test_result <- kruskal.test(resignature_data[[gene]] ~ resignature_data$Call)
-#   
-#   # If Kruskal-Wallis test is significant, perform Dunn's test
-#   significant_pairs <- NA_character_
-#   if (test_result$p.value < 0.05) {
-#     dunn_result <- dunnTest(resignature_data[[gene]] ~ resignature_data$Call, method = "bh")
-#     
-#     # Check column names and extract significant pairwise comparisons
-#     if ("Comparison" %in% colnames(dunn_result$res) & "P.adj" %in% colnames(dunn_result$res)) {
-#       sig_pairs <- dunn_result$res %>%
-#         filter(P.adj < 0.05) %>%
-#         pull(Comparison)  # Extract significant pair names
-#       
-#       # Store significant comparisons as a comma-separated string
-#       if (length(sig_pairs) > 0) {
-#         significant_pairs <- paste(sig_pairs, collapse = ", ")
-#       }
-#     }
-#   }
-#   
-#   # Append results to module_results dataframe
-#   module_results <- rbind(module_results, 
-#                           data.frame(Gene = gene, 
-#                                      p_value = test_result$p.value, 
-#                                      significant_pairs = significant_pairs))
-# }
-# 
-# # Adjust p-values using Benjamini-Hochberg correction
-# module_results$p_adj <- p.adjust(module_results$p_value, method = "BH")
-# 
-# # Subset significant module_results after adjustment (e.g., FDR < 0.05)
-# significant_module_results <- module_results %>% filter(p_adj < 0.05)
-# 
-# # Reordering significant module_results
-# significant_module_results <- significant_module_results %>%
-#   mutate(PMID = ifelse(grepl("_PMID\\.\\d+$", Gene), 
-#                        sub(".*_PMID\\.", "", Gene), 
-#                        NA_character_)) %>%
-#   arrange(desc(!is.na(PMID)), PMID)
-# # Saving significant module_results as a excel file
-# write_xlsx(significant_module_results, "sTILs_gene_signatures.xlsx")
 
 
 # Solo testing individual signatures
