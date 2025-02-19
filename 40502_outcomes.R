f2fb19d09b8c555a737c74263a04f22dc7fcccad
eblige01 <blige.1@osu.edu>
2025-02-19 09:56:29 -0500

    Adding Responder vs Non responder wilcoxon for decon

--- 40502_outcomes.R
+++ 40502_outcomes.R
@@ -26,4 +26,46 @@ mergeddata$rna_decon_sampleid <- gsub("Sample_", "sample",mergeddata$rna_decon_s
 pac_sub <- mergeddata %>% filter(arm == 1 , strat2_recep == 1)
 
 
-pac_sub <- merge(trans_rna_seq_df,pac_sub[c("survmos","pfsmos","pfsstat","survstat","rna_decon_sampleid")],by = "rna_decon_sampleid")
+pac_sub <- merge(trans_rna_seq_df,pac_sub[c("bestresp","rna_decon_sampleid")],by = "rna_decon_sampleid")
+
+# Removing patients with no tumor response data
+pac_sub <- na.omit(pac_sub)
+
+# Creating response column 
+pac_sub$responder_stat <- ifelse(pac_sub$bestresp <= 2,"responder","nonresponder")
+
+# # Response Vs Decon PAc and HR+
+# # Initialize an empty dataframe to store module_module_results
+# rna_results <- data.frame(Cell = character(), p_value = numeric(), stringsAsFactors = FALSE,higher_expression = character())
+# 
+# # Get the gene column names (excluding sample_id and TIL_group)
+# cell_cols <- setdiff(names(pac_sub), c("rna_decon_sampleid", "bestresp","responder_stat"))
+# 
+# # Loop through each gene column
+# for (cell in cell_cols) {
+#   # Ensure the column is numeric
+#   pac_sub[[cell]] <- as.numeric(pac_sub[[cell]])
+#   # Perform Wilcoxon test (Mann-Whitney U test)
+#   test_result <- wilcox.test(pac_sub[[cell]] ~ pac_sub$responder_stat)
+# 
+#   # Calculate mean expression for each group will be used to determine the dirrection of sig module_results
+#   group_means <- pac_sub %>%
+#     group_by(responder_stat) %>%
+#     summarise(mean_expression = mean(!!sym(cell), na.rm = TRUE)) %>%
+#     arrange(desc(mean_expression))
+# 
+#   # Determine which group has higher expression
+#   higher_group <- group_means$responder_stat[1]
+# 
+#   # Append module_results to dataframe
+#   rna_results <- rbind(rna_results, data.frame(Cell = cell, p_value = test_result$p.value,higher_expression = higher_group))
+# }
+# 
+# rna_results$p_adj <- p.adjust(rna_results$p_value, method = "bonferroni")
+# 
+# # Subset significant module_results after adjustment (e.g., FDR < 0.05)
+# significant_rna_results <- rna_results %>% filter(p_adj < 0.05)
+# sig_cells <- significant_rna_results$Cell
+# #Saving results
+# write_xlsx(rna_results, "response_pac_decon.xlsx")
+
