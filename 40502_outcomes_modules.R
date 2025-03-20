3435aabaf1fd5b119cd81371d887ef7898ee7365
eblige01 <blige.1@osu.edu>
2025-03-20 11:02:29 -0400

    Adding OR for response calculations for modules

--- 40502_outcomes_modules.R
+++ 40502_outcomes_modules.R
@@ -1,6 +1,9 @@
 library("dplyr")
 library("broom")
 library("ggplot2")
+library("biglm") 
+library("pbapply")
+
 # 40502 Outcome script
 M40502_joined_metadata <- read.csv("C:\\Users\\blig02\\OneDrive - The Ohio State University Wexner Medical Center\\40502_data\\Data\\40502_joined_metadata_fixed.csv", dec=",")
 D40502_data <- read.csv("C:\\Users\\blig02\\OneDrive - The Ohio State University Wexner Medical Center\\40502_data\\Data\\NCTN-D3-recoded.csv", dec=",")
@@ -32,16 +35,33 @@ mergeddata <- merge(resignature_data,mergeddata[, c("INVESTIGATOR_SAMPLENAME", "
 mergeddata <-  mergeddata %>% filter(!is.na(bestresp))
 
 #Adding binary outcome for pCR
-mergeddata$pCR <- ifelse(mergeddata$bestresp == 1, 1,0)
+mergeddata$pCR <- ifelse(mergeddata$bestresp %in% c(1,2), 1,0)
 
 #Extracting signatures from data frame
 features <- colnames(mergeddata)[!(colnames(mergeddata) %in% c ("INVESTIGATOR_SAMPLENAME","bestresp","pCR"))]
 
 # logistic regression for all signatures
-results <- lapply(features,function(f) {
-  formula <- as.formula(paste("pCR ~",f))
-  model <- glm(formula,data =mergeddata,family = "binomial" )
-  tidy_model <- tidy(model,conf.int = TRUE, exponentiate = TRUE)
+
+results <- pblapply(features, function(f) {
+  formula <- as.formula(paste("pCR ~", f))
+  model <- bigglm(formula, data = mergeddata, family = binomial())
+  tidy_model <- broom::tidy(model, conf.int = TRUE, exponentiate = TRUE)
   tidy_model$term <- f
   return(tidy_model[2,])
 })
+
+pcr_or_df <- do.call(rbind,results)
+
+# Extract the signature you want to calculate OR for
+feature <- "Up_regulated_upon_N_RAS_repression_Cell_Rep.2015_PMID.26166574"  # Replace this with the specific feature/column name you want
+
+# Logistic regression for the chosen signature
+formula <- as.formula(paste("pCR ~", feature))
+model <- bigglm(formula, data = mergeddata, family = binomial())
+
+# Tidy model to get OR, CI, and p-value
+tidy_model <- broom::tidy(model, conf.int = TRUE, exponentiate = TRUE)
+
+# Extract and print OR (odds ratio) and its confidence interval for the chosen feature
+tidy_model$term <- feature
+print(tidy_model[2, ])  # The 2nd row corresponds to the OR for the feature
