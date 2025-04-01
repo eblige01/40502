2c725b6924583dd7185e6dd3900297d4a3f905ef
eblige01 <blige.1@osu.edu>
2025-04-01 16:14:00 -0400

    cleaning metadata

--- 40502_outcomes_modules.R
+++ 40502_outcomes_modules.R
@@ -6,15 +6,17 @@ M40502_joined_metadata <- read_csv("C:\\Users\\blig02\\OneDrive - The Ohio State
 D40502_data <- read_csv("C:\\Users\\blig02\\OneDrive - The Ohio State University Wexner Medical Center\\40502_data\\Data\\NCTN-D3-recoded.csv")
 signature_data <- read_table("C:\\Users\\blig02\\OneDrive - The Ohio State University Wexner Medical Center\\40502_data\\Data\\cdt.txt")
 
-# Removing rows with NA for Investigator_SAMPLENAME
-M40502_joined_metadata <- M40502_joined_metadata |> 
-  filter(!(is.na(INVESTIGATOR_SAMPLENAME)))
 
 # Removing rows from data that do not have patids in the metadata
 D40502_data <- D40502_data |> 
   filter(patid %in% M40502_joined_metadata$patid)
 
-# Removing duplicate
+M40502_joined_metadata <- M40502_joined_metadata |> 
+  left_join(D40502_data, by = "patid") |> 
+  relocate(bestresp, .after = rna_decon_sampleid ) |> 
+  select(patid:bestresp) |> 
+  filter(!(is.na(bestresp)) & !(is.na(INVESTIGATOR_SAMPLENAME)))
+
 # Making signature data tidy by removing extra columns and rows
 tidy_signature_data <- signature_data |>  
   select(-c("GID","CLID","GWEIGHT")) |> 
@@ -27,13 +29,9 @@ colnames(tidy_signature_data) <- str_remove(colnames(tidy_signature_data),"^X")
 # Removing columns not found in metadata
 tidy_signature_data <- select(tidy_signature_data,any_of(M40502_joined_metadata$INVESTIGATOR_SAMPLENAME))
 
-match(D40502_data$patid,M40502_joined_metadata$patid)
 
-# Use mutate to create bestrep in M4 so we dont lose data due to duplicate paitent IDS
 
-M40502_joined_metadata <- M40502_joined_metadata |> 
-  left_join(D40502_data, by = "patid") 
-  
+
 
 
           
