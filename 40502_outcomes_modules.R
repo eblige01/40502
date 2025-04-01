104651e8b797dbd24fc766718cf5af0ea4dc1206
eblige01 <blige.1@osu.edu>
2025-04-01 14:28:03 -0400

    Completing rewriting code to make cleaner and to inclue more data points.

--- 40502_outcomes_modules.R
+++ 40502_outcomes_modules.R
@@ -1,67 +1,37 @@
-library("dplyr")
+library("tidyverse")
 library("broom")
-library("ggplot2")
 library("biglm") 
 library("pbapply")
 
-# 40502 Outcome script
-M40502_joined_metadata <- read.csv("C:\\Users\\blig02\\OneDrive - The Ohio State University Wexner Medical Center\\40502_data\\Data\\40502_joined_metadata_fixed.csv", dec=",")
-D40502_data <- read.csv("C:\\Users\\blig02\\OneDrive - The Ohio State University Wexner Medical Center\\40502_data\\Data\\NCTN-D3-recoded.csv", dec=",")
-signature_data <- read.table("C:\\Users\\blig02\\OneDrive - The Ohio State University Wexner Medical Center\\40502_data\\Data\\cdt.txt", header = TRUE, sep = "\t", comment.char = "", quote = "")
+# 40502 Outcome modules
+M40502_joined_metadata <- read_csv("C:\\Users\\blig02\\OneDrive - The Ohio State University Wexner Medical Center\\40502_data\\Data\\40502_joined_metadata_fixed.csv")
+D40502_data <- read_csv("C:\\Users\\blig02\\OneDrive - The Ohio State University Wexner Medical Center\\40502_data\\Data\\NCTN-D3-recoded.csv")
+signature_data <- read_table("C:\\Users\\blig02\\OneDrive - The Ohio State University Wexner Medical Center\\40502_data\\Data\\cdt.txt")
 
-# Reformatting signature data for analysis
-# Removing unnecessary rows and coloumns
-resignature_data <- signature_data %>% select(-c("GID","CLID","GWEIGHT"))
-resignature_data <- resignature_data %>% slice(-c(1,2))
+# Removing rows with NA for Investigator_SAMPLENAME
+M40502_joined_metadata <- M40502_joined_metadata |> 
+  filter(!(is.na(INVESTIGATOR_SAMPLENAME)))
 
-# Transposing the dataframe
-resignature_data <- t(resignature_data)
+# Removing rows from data that do not have patids in the metadata
+D40502_data <- D40502_data |> 
+  filter(patid %in% M40502_joined_metadata$patid)
 
-# Making first row colnames and making rownames a column
-colnames(resignature_data) <- resignature_data[1,]
-resignature_data <- resignature_data[-1,]
-resignature_data <- as.data.frame(resignature_data)
-resignature_data$INVESTIGATOR_SAMPLENAME <- rownames(resignature_data)
+# Removing duplicate
+# Making signature data tidy by removing extra columns and rows
+tidy_signature_data <- signature_data |>  
+  select(-c("GID","CLID","GWEIGHT")) |> 
+  slice(-c(1,2)) |>
+  column_to_rownames("NAME") 
+  
+# Removing leading X to match the metadata
+colnames(tidy_signature_data) <- str_remove(colnames(tidy_signature_data),"^X")
 
-# Moving INVESTIGATOR_SAMPLENAME for to the from for convenience
-resignature_data <- resignature_data[, c("INVESTIGATOR_SAMPLENAME", setdiff(names(resignature_data), "INVESTIGATOR_SAMPLENAME"))]
+# Removing columns not found in metadata
+tidy_signature_data <- select(tidy_signature_data,any_of(M40502_joined_metadata$INVESTIGATOR_SAMPLENAME))
 
-# Reformatting and merging
-resignature_data <- resignature_data %>% mutate(INVESTIGATOR_SAMPLENAME = sub("^X", "", INVESTIGATOR_SAMPLENAME))
-mergeddata <- merge(D40502_data, M40502_joined_metadata[, c("patid","INVESTIGATOR_SAMPLENAME")], by = "patid")
-mergeddata <- merge(resignature_data,mergeddata[, c("INVESTIGATOR_SAMPLENAME", "bestresp")],by = "INVESTIGATOR_SAMPLENAME")
+match(D40502_data$patid,M40502_joined_metadata$patid)
 
-# Removing values with no valesu for bestresp
-mergeddata <-  mergeddata %>% filter(!is.na(bestresp))
+# Use mutate to create bestrep in M4 so we dont lose data due to duplicate paitent IDS
 
-#Adding binary outcome for pCR
-mergeddata$pCR <- ifelse(mergeddata$bestresp %in% c(1,2), 1,0)
 
-#Extracting signatures from data frame
-features <- colnames(mergeddata)[!(colnames(mergeddata) %in% c ("INVESTIGATOR_SAMPLENAME","bestresp","pCR"))]
 
-# logistic regression for all signatures
-
-results <- pblapply(features, function(f) {
-  formula <- as.formula(paste("pCR ~", f))
-  model <- bigglm(formula, data = mergeddata, family = binomial())
-  tidy_model <- broom::tidy(model, conf.int = TRUE, exponentiate = TRUE)
-  tidy_model$term <- f
-  return(tidy_model[2,])
-})
-
-pcr_or_df <- do.call(rbind,results)
-
-# Extract the signature you want to calculate OR for
-feature <- "Up_regulated_upon_N_RAS_repression_Cell_Rep.2015_PMID.26166574"  # Replace this with the specific feature/column name you want
-
-# Logistic regression for the chosen signature
-formula <- as.formula(paste("pCR ~", feature))
-model <- bigglm(formula, data = mergeddata, family = binomial())
-
-# Tidy model to get OR, CI, and p-value
-tidy_model <- broom::tidy(model, conf.int = TRUE, exponentiate = TRUE)
-
-# Extract and print OR (odds ratio) and its confidence interval for the chosen feature
-tidy_model$term <- feature
-print(tidy_model[2, ])  # The 2nd row corresponds to the OR for the feature
